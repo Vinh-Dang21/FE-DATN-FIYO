6d8c428b687818106f0a6b9a65e54155749ab329
Nguyễn Duy Phương <phuongndps39487@gmail.com>
2025-09-19 15:06:51 +0700

    ok

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: true, // 301 redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
